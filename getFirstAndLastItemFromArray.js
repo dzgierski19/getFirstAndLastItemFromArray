8f0c5f4aedf1a9c24fb8644db1340477a961b825
dzgierski19 <dzgierskigit@gmail.com>
2023-03-23 21:41:18 +0100

    validation added

--- getFirstAndLastItemFromArray.js
+++ getFirstAndLastItemFromArray.js
@@ -1,8 +1,25 @@
 const array = [1, 2, 3, 4, 5];
 
 const getFirstAndLastItemFromArray = (array) => {
+  isArrayAnArray(array);
+  isArrayNotEmpty(array);
   const firstItem = array.splice(0, 1);
   const lastItem = array.splice(-1, 1);
 
   return "FirstElement:" + firstItem + "LastItem:" + lastItem;
 };
+
+const isArrayAnArray = (input) => {
+  if (!Array.isArray(input)) {
+    throw new Error("You must type an Array");
+  }
+};
+
+const isArrayNotEmpty = (input) => {
+  if (input.length === 0) {
+    throw new Error("You must not type an empty Array");
+  }
+};
+
+const result1 = getFirstAndLastItemFromArray(array);
+console.log(result1);
